c9e1863979a4ca193dd2b70f1ae735f3bd8abc83
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-30 16:32:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,7 @@
 class Dog 
   def initialize (name, breed)
     @name = name
-    @breed = breed_meth
-  end
-  
-  def breed_meth(breed=nil)
-    breed ||= "Mutt"
+    @breed = breed ||= "Mutt"
   end
   
 end 
